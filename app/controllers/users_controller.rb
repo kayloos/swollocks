c2594b24dca4e393e8330ee44e6f9d8eb9f216fb
Kayloos <kalusn@gmail.com>
2010-07-20 21:25:21 +0200

    commititnng

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,34 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @title = @user.name
   end
 
   def new
     @title = "Sign up"
+    @user = User.new
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def sign_in
+    @title = "Sign in"
+
+    if params[:email] then
+      email = params[:email]
+      password = params[:password]
+      if user = User.authenticate(email, password) then
+        session[:user] = user
+      else
+        @title = "YOU FAIL"
+      end
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.signup  '/signup',  :controller => 'users', :action => 'new'
+  map.sign_in '/sign_in', :controller => 'users', :action => 'sign_in'
   map.resources :users
   # The priority is based upon order of creation: first created -> highest priority.
 
