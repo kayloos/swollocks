d687281fe096ac0973ba9507eac6532077d376d7
Kayloos <kalusn@gmail.com>
2010-07-30 00:02:24 +0200

    All users done, protected pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :show, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -25,12 +32,32 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
+  def edit
+    @title = "Edit user"
+  end
 
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
   end
 
   def destroy
 
   end
 
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
