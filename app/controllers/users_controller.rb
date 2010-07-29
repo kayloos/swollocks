93e4ca6243e07cf7272eaef5d6aa003db9fb5c54
Kayloos <kalusn@gmail.com>
2010-07-30 00:29:49 +0200

    Done admin and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :show, :edit, :update]
-  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:show, :edit, :update]
+  before_filter :admin_user,   :only => [:destroy]
 
   def index
     @title = "All users"
@@ -48,6 +49,10 @@ class UsersController < ApplicationController
 
   def destroy
 
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+
   end
 
   private
@@ -58,6 +63,10 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to(root_path) unless current_user?(@user) || current_user.admin?
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100726133641) do
+ActiveRecord::Schema.define(:version => 20100729220602) do
 
   create_table "lists", :force => true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20100726133641) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.string   "remember_token"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
