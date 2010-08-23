42e8d5019d11d9a630eeee9fad2d6eb488282409
Claus Skou Nielsen <claus@Claus-Skou-Nielsens-MacBook.local>
2010-08-24 00:04:07 +0200

    fixed the mailsender

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,20 @@ module ApplicationHelper
     end
   end
 
+# this adds proper whitespace to numbers (which is beforehand converted to strings)
+  def add_whitespace(string_nr)
+    string_nr.to_s
+    if string_nr.length >= 3
+      counter = 4
+      stop = false
+      while stop == false
+        string_nr = string_nr.insert(-counter, ' ')
+        if string_nr.index(' ') <= 3
+          stop = true
+        end
+        counter += 4
+      end
+    end
+    return string_nr
+  end
 end
--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,11 +1,20 @@
 class UserMailer < ActionMailer::Base
-
+	helper ApplicationHelper 
   def stock_update(user)
     recipients    user.email
     from          "Lisduck Stock Notifications <notifications@lisduck.dk>"
     subject       "Welcome to My Awesome Site"
     sent_on       Time.now
-    body          :name => user.name
+  	stock_data = Hash.new
+  	list = List.find_all_by_user_id(user.id)
+  	list.each do |l|
+  		stock_data[l.name] = Array.new
+  		stock = Stock.find_all_by_list_id(l.id)
+  		stock.each do |s|
+  			stock_data[l.name] << StockYank.find(s.stock_id).attributes
+  		end
+  	end
+    body          :name => user.name, :stock_data => stock_data
   end
 
 end
