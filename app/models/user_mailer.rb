fb9f3d96edb0a4a8247508c2cf5fcf7724f828cb
Claus Skou Nielsen <claus@Claus-Skou-Nielsens-MacBook.local>
2010-08-23 21:20:39 +0200

    J

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
 
-  def stock_update(user)
-    recipients    user.email
+  def stock_update
+    recipients    "kalusn@gmail.com"
     from          "Lisduck Stock Notifications <notifications@lisduck.dk>"
     subject       "Welcome to My Awesome Site"
     sent_on       Time.now
