d313c5784d8e8db08fa43fa9acfc970c673da4bb
Claus Skou Nielsen <claus@kalusns-comp00tah.local>
2010-10-26 00:17:40 +0200

    Shit has hit the fan

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,2 @@
 module ApplicationHelper
-
-  # Return a title on a per-page basis.
-  def title
-    base_title = "Børsboss"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} - #{h(@title)}"
-    end
-  end
-
-# this adds proper whitespace to numbers (which is beforehand converted to strings)
-  def add_whitespace(string_nr)
-    string_nr = string_nr.to_s
-    if string_nr.length >= 3
-      counter = 4
-      stop = false
-      while stop == false
-        string_nr = string_nr.insert(-counter, ' ')
-        if string_nr.index(' ') <= 3
-          stop = true
-        end
-        counter += 4
-      end
-    end
-    string_nr
-  end
 end
