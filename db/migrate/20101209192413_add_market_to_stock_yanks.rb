7142e58b7025154ea7c3888ddb83b3c35d4045dd
Claus Skou Nielsen <kalusn@gmail.com>
2010-12-11 12:27:09 +0100

    Populating for production

--- db/migrate/20101209192413_add_market_to_stock_yanks.rb
+++ db/migrate/20101209192413_add_market_to_stock_yanks.rb
@@ -1,7 +1,7 @@
 class AddMarketToStockYanks < ActiveRecord::Migration
   def self.up
-    add_column :stock_yanks, :market_id, :integer
-    add_column :yank_proposals, :market_id, :integer
+    add_column :stock_yanks, :market_id, :integer, :default => 1
+    add_column :yank_proposals, :market_id, :integer, :default => 1
     StockYank.all.each do |sy|
       sy.market_id = 1
       sy.save
