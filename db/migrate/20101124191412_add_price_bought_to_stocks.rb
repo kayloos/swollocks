72e1c0e19a196e9af1992a62782acc781deba95e
Claus Skou Nielsen <kalusn@gmail.com>
2010-12-12 19:46:39 +0100

    Precision on money db columns set to 10/12

--- db/migrate/20101124191412_add_price_bought_to_stocks.rb
+++ db/migrate/20101124191412_add_price_bought_to_stocks.rb
@@ -1,6 +1,6 @@
 class AddPriceBoughtToStocks < ActiveRecord::Migration
   def self.up
-    add_column :stocks, :bought_at, :decimal, :default => 0, :precision => 8, :scale => 2
+    add_column :stocks, :bought_at, :decimal, :default => 0, :precision => 12, :scale => 2
   end
 
   def self.down
--- db/migrate/20101204180846_create_trades.rb
+++ db/migrate/20101204180846_create_trades.rb
@@ -5,7 +5,7 @@ class CreateTrades < ActiveRecord::Migration
       t.string :action
       t.string :portfolio_name
       t.string :stock_name
-      t.decimal :traded_at, :default => 0, :precision => 8, :scale => 2
+      t.decimal :traded_at, :default => 0, :precision => 10, :scale => 2
       t.decimal :amount
 
       t.timestamps
