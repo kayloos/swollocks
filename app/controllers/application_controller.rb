4cd810e22485fe66922cec1a44ca0424df334edc
Claus Skou Nielsen <kalusn@gmail.com>
2011-01-29 08:04:32 +0100

    added meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   include SessionsHelper
   include StocksHelper
+  before_filter :meta_defaults
+
+  def meta_defaults
+    @meta_description = 'Analyze life on the stock market with Swollocks'
+    @meta_keywords = 'stocks, stock, aktier, aktie, aktiespil, stock analysis'
+  end
 end
