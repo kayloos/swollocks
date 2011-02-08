230b9bea3007f64b1f4c87901aaeabbb6d8c0512
Claus Skou Nielsen <kalusn@gmail.com>
2011-02-09 00:34:08 +0100

    Added simplemodal to front page

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -1,32 +1,16 @@
 $(function(){
   // FRONT PAGE JS FOR SIGNIN SIGNUP
   $(".signin").click(function(e) {
-    e.preventDefault();
-    $("#signin_box").fadeIn(400,function(){
-      $("#session_email").focus();
-    });
-    $("#box_focus").fadeIn(400);
+    $("#signin_box").modal({modal:true,
+                            overlayClose:true});
+    $("#session_email").focus();
+    return false;
   });
   
   $(".signup").click(function(e) {
-      e.preventDefault();
-      $("#signup_box").fadeIn(400, function(){
-        $("#user_name").focus();
-      });
-      $("#box_focus").fadeIn(400);
-    });
-  
-  // Mask box when fading the background to black (with opacity)
-  $("#box_focus").click(function() {
-    $("#signin_box").fadeOut(200);
-    $("#signup_box").fadeOut(200);
-    $(".trade").fadeOut(200);
-    $(this).fadeOut(200);
+    $("#signup_box").modal({modal:true,
+                            overlayClose:true});
+    $("#user_name").focus();
+    return false;
   });
-  
-  var resizeBox = function() {
-    $("#box_focus").height("100%");
-  };
-  resizeBox();
-  $(window).resize(resizeBox);
 });
